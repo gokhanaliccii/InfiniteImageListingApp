3b6f83f7fa91d2f029571fbab51658de4f1a5b85
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-07 20:49:49 +0300

    Json parser generic object unit test added

--- jsonparser/src/main/java/com/gokhanaliccii/jsonparser/JsonParser.kt
+++ jsonparser/src/main/java/com/gokhanaliccii/jsonparser/JsonParser.kt
@@ -1,4 +1,12 @@
 package com.gokhanaliccii.jsonparser
 
 class JsonParser {
+
+    fun <T> parse(json: String, clazz: Class<T>): T? {
+        clazz.constructors
+
+        return clazz.classLoader.loadClass(clazz.name).newInstance()  as T
+    }
+
+
 }
--- jsonparser/src/test/java/com/gokhanaliccii/jsonparser/JsonParserTest.kt
+++ jsonparser/src/test/java/com/gokhanaliccii/jsonparser/JsonParserTest.kt
@@ -1,5 +1,19 @@
 package com.gokhanaliccii.jsonparser
 
 import org.junit.Assert.*
+import org.junit.Test
+import java.lang.reflect.Constructor
 
-class JsonParserTest
+class JsonParserTest {
+
+    @Test
+    fun shouldParsedObjectNotNull() {
+        val input = "{\"name\":\"gokhan\",\"age\":1}"
+        val person = JsonParser().parse(input, Person::class.java)
+
+        assertNotNull(person)
+    }
+
+     class Person
+
+}
