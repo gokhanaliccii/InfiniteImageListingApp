725795f13aace13681e69a6b96912b1c2ad0b605
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-07 08:46:06 +0300

    Http delegation classes such as HttpRequest,HttpRequestQueue created

---
+++ httpclient/src/main/java/com/gokhanaliccii/httpclient/HttpRequestQueue.kt
@@ -0,0 +1,33 @@
+package com.gokhanaliccii.httpclient
+
+import java.lang.reflect.Type
+
+interface HttpRequestQueue<T> {
+
+    fun <T> add(info: HttpRequest<T>, needCache: Boolean)
+
+    fun <T> add(info: HttpRequest<T>, needCache: Boolean, shouldUseRetry: Boolean)
+
+    fun remove(tag: String)
+
+    fun removeAll()
+
+    fun start()
+
+    fun stop()
+
+    class HttpRequest<T>(val requestInfo: HttpRequestInfo, val type: Type, val resultListener: HttpResult<T>)
+
+    class HttpRequestInfo(val url: String, val method: Method) {
+        var body: Any? = null
+        lateinit var headers: Map<String, String>
+
+        enum class Method {
+            GET, POST
+        }
+    }
+
+    interface HttpResult<T> {
+        fun onResponse(response: T)
+    }
+}
