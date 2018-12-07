26f0bffa49b7235781775f85a0f605eece4d338b
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-07 23:56:07 +0300

    Jsonparser field parsing functionality added as well as unit test

--- jsonparser/src/test/java/com/gokhanaliccii/jsonparser/JsonParserTest.kt
+++ jsonparser/src/test/java/com/gokhanaliccii/jsonparser/JsonParserTest.kt
@@ -13,6 +13,29 @@ class JsonParserTest {
         assertNotNull(person)
     }
 
+    @Test
+    fun shouldParseStringFieldCorrectly() {
+        val expectedName = "gokhan"
+        val input = "{\"name\":\"gokhan\",\"age\":1}"
+        val person = JsonParser().parse(input, NamedPerson::class.java)
+
+        assertTrue(expectedName == person?.name)
+    }
+
+    /* @Test
+     fun shouldParseJsonCorrectly() {
+         val expectedName = "gokhan"
+         val input = "{\"name\":\"gokhan\",\"age\":1,\"friends\":[{\"name\":\"ahmet\"}]}"
+         val student = JsonParser().parse(input, Student::class)
+
+         assertTrue(expectedName == student?.name)
+     }*/
+
+
+    class Friend(val name: String)
+
+    class Student(val name: String, val age: Int, val friends: List<Friend>)
+
     class NamedPerson {
         lateinit var name: String
     }
