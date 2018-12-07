81e3146339e192f64625f10e10d7cca780188d50
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-07 08:58:40 +0300

    Http url annotation created

---
+++ httpclient/src/main/java/com/gokhanaliccii/httpclient/request/url/Path.kt
@@ -0,0 +1,7 @@
+package com.gokhanaliccii.httpclient.request.url
+
+
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class Path(val value: String = "")
