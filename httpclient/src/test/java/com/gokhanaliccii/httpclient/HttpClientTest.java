46d8a93c51274304f869227de321b59add27ed4f
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-08 14:11:05 +0300

    Service url creation with query param unit test added

--- httpclient/src/test/java/com/gokhanaliccii/httpclient/HttpClientTest.java
+++ httpclient/src/test/java/com/gokhanaliccii/httpclient/HttpClientTest.java
@@ -2,12 +2,14 @@ package com.gokhanaliccii.httpclient;
 
 import com.gokhanaliccii.httpclient.HttpRequestQueue.HttpRequestInfo.Method;
 import com.gokhanaliccii.httpclient.annotation.method.GET;
+import com.gokhanaliccii.httpclient.annotation.url.Query;
 import com.gokhanaliccii.httpclient.util.MockHttpRequestQueue;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class HttpClientTest {
 
@@ -40,9 +42,23 @@ public class HttpClientTest {
         assertThat(info.getUrl(), is(expectedUrl));
     }
 
+    @Test
+    public void should_QueryParamCorrect() {
+        final String name = "Name1=Value1";
+        final String expectedUrl = BASE_URL + "/getMethodWithParams?" + name;
+        Service service =
+                HttpClient.with(queue, BASE_URL).create(Service.class);
+
+        Request<String> call = service.getMethodWithQueryParam("Value1");
+        HttpRequestQueue.HttpRequestInfo info = call.getRequestInfo();
+        assertThat(info.getUrl(), is(equalTo(expectedUrl)));
+    }
 
     interface Service {
         @GET("/getMethod")
         Request<String> getMethod();
+
+        @GET("/getMethodWithParams")
+        Request<String> getMethodWithQueryParam(@Query("Name1") String name);
     }
 }
