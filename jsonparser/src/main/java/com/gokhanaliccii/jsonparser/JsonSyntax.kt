038c0a99a535a1f89e112baac9e2dc2ceaebb401
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-08 12:54:32 +0300

    Unused functions removed, jsonObject and jsonArray detection functions diverged

--- jsonparser/src/main/java/com/gokhanaliccii/jsonparser/JsonSyntax.kt
+++ jsonparser/src/main/java/com/gokhanaliccii/jsonparser/JsonSyntax.kt
@@ -6,15 +6,6 @@ const val JSON_OBJECT_END = "}"
 const val JSON_ARRAY_START = "["
 const val JSON_ARRAY_END = "]"
 
-internal fun String.hasValidSyntax(): Boolean {
-    return startsWith(JSON_ARRAY_START) && endsWith(JSON_ARRAY_END)
-            || startsWith(JSON_OBJECT_START) && endsWith(JSON_OBJECT_END)
-}
+internal fun String.hasValidObjectSyntax(): Boolean = startsWith(JSON_OBJECT_START) && endsWith(JSON_OBJECT_END)
 
-internal fun String.isJsonObject(): Boolean {
-    return startsWith(JSON_OBJECT_START)
-}
-
-internal fun String.isJsonArray(): Boolean {
-    return startsWith(JSON_ARRAY_START)
-}
+internal fun String.hasValidArraySyntax(): Boolean = startsWith(JSON_ARRAY_START) && endsWith(JSON_ARRAY_END)
