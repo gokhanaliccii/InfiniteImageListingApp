f607087928964c077d25ad62c1e31d5e8ecddb88
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-08 22:11:38 +0300

    Http requests send via executors and responses notifyed over the main thread

--- httpclient/src/main/java/com/gokhanaliccii/httpclient/JsonRequestQueue.kt
+++ httpclient/src/main/java/com/gokhanaliccii/httpclient/JsonRequestQueue.kt
@@ -1,5 +1,6 @@
 package com.gokhanaliccii.httpclient
 
+import android.os.Looper
 import com.gokhanaliccii.httpclient.util.closeStreams
 import com.gokhanaliccii.httpclient.util.isResponseCodeSucceed
 import com.gokhanaliccii.jsonparser.jsonTo
@@ -8,12 +9,14 @@ import java.io.ByteArrayOutputStream
 import java.net.HttpURLConnection
 import java.net.ProtocolException
 import java.net.URL
-
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class JsonRequestQueue : HttpRequestQueue {
 
-    override fun <T> add(request: HttpRequestQueue.HttpRequest<T>, needCache: Boolean) {
+    private val executorService: ExecutorService by lazy { Executors.newFixedThreadPool(10) }
 
+    override fun <T> add(request: HttpRequestQueue.HttpRequest<T>, needCache: Boolean) {
     }
 
     override fun <T> add(request: HttpRequestQueue.HttpRequest<T>, needCache: Boolean, shouldUseRetry: Boolean) {
@@ -21,43 +24,44 @@ class JsonRequestQueue : HttpRequestQueue {
     }
 
     override fun start() {
-
     }
 
     override fun stop() {
-
     }
 
     override fun removeAll() {
-
     }
 
     private fun <T> sendRequest(request: HttpRequestQueue.HttpRequest<T>) {
         val requestInfo = request.requestInfo
 
-        try {
-            val connection = URL(requestInfo.url).openConnection() as HttpURLConnection
-            putHeaders(requestInfo, connection)
-            putHttpMethod(requestInfo, connection)
+        executorService.submit {
+            try {
+                val connection = URL(requestInfo.url).openConnection() as HttpURLConnection
+                putHeaders(requestInfo, connection)
+                putHttpMethod(requestInfo, connection)
 
-            if (connection.responseCode.isResponseCodeSucceed()) {
-                val outputStream = ByteArrayOutputStream()
-                val inputStream = connection.inputStream
-                inputStream.copyTo(outputStream)
-                closeStreams(inputStream, outputStream)
+                if (connection.responseCode.isResponseCodeSucceed()) {
+                    val outputStream = ByteArrayOutputStream()
+                    val inputStream = connection.inputStream
+                    inputStream.copyTo(outputStream)
+                    closeStreams(inputStream, outputStream)
 
-                val rawJson = outputStream.toString()
-                val returnType = requestInfo.returnType!!
-                val isReturnTypeArray = requestInfo.isArray!!
+                    val rawJson = outputStream.toString()
+                    val returnType = requestInfo.returnType!!
+                    val isReturnTypeArray = requestInfo.isArray!!
 
-                if (isReturnTypeArray) {
-                    request.resultListener.onResponse(rawJson.jsonTo(returnType))
-                } else {
-                    request.resultListener.onResponse(rawJson.jsonToList(returnType))
+                    android.os.Handler(Looper.getMainLooper()).post {
+                        if (isReturnTypeArray) {
+                            request.resultListener.onResponse(rawJson.jsonToList(returnType))
+                        } else {
+                            request.resultListener.onResponse(rawJson.jsonTo(returnType))
+                        }
+                    }
                 }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
 
     }
