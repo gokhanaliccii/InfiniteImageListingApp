fd04a923a238c8162774a968e4b09aa039243d1c
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-08 14:01:57 +0300

    HttpClient class created to manage http request.
    It internally process function annotations to figure out requested service

---
+++ httpclient/src/main/java/com/gokhanaliccii/httpclient/HttpClient.java
@@ -0,0 +1,75 @@
+package com.gokhanaliccii.httpclient;
+
+import java.lang.reflect.*;
+
+import static com.gokhanaliccii.httpclient.HttpClientUtils.hasUnresolvableType;
+
+public class HttpClient {
+    private final String baseUrl;
+    private final HttpRequestQueue queue;
+
+    private HttpClient(HttpRequestQueue queue, String baseUrl) {
+        this.queue = queue;
+        this.baseUrl = baseUrl;
+    }
+
+    public static HttpClient with(HttpRequestQueue queue, String baseUrl) {
+        return new HttpClient(queue, baseUrl);
+    }
+
+    public <T> T create(final Class<T> service) {
+        validateServiceInterface(service);
+
+        return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class<?>[]{service},
+                new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args)
+                            throws Throwable {
+
+                        if (method.getDeclaringClass() == Object.class) {
+                            return method.invoke(this, args);
+                        }
+
+                        HttpRequestQueue.HttpRequestInfo requestInfo =
+                                RequestInfoConverter.newInstance()
+                                        .javaMethod(method)
+                                        .baseUrl(baseUrl)
+                                        .args(args)
+                                        .compile();
+
+                        Type type = getReturnType(method);
+                        Type returnType = HttpClientUtils.getParameterUpperBound(
+                                0, (ParameterizedType) type);
+
+                        return new Request<>(queue, requestInfo, returnType);
+                    }
+                });
+    }
+
+    private <T> void validateServiceInterface(Class<T> service) {
+        if (!service.isInterface()) {
+            throw new IllegalArgumentException("Service declaration must be interfaces.");
+        }
+
+        if (service.getInterfaces().length > 0) {
+            throw new IllegalArgumentException("Service must not extend other interfaces.");
+        }
+
+    }
+
+    private Type getReturnType(Method javaMethod) {
+        Type returnType = javaMethod.getGenericReturnType();
+
+        if (hasUnresolvableType(returnType)) {
+            throw new RuntimeException(
+                    "Method return type must not include a type variable or wildcard: "
+                            + returnType);
+        }
+
+        if (returnType == Void.class) {
+            throw new RuntimeException("Service methods cannot return void");
+        }
+
+        return returnType;
+    }
+}
--- httpclient/src/test/java/com/gokhanaliccii/httpclient/HttpClientTest.java
+++ httpclient/src/test/java/com/gokhanaliccii/httpclient/HttpClientTest.java
@@ -1,5 +1,36 @@
-import static org.junit.Assert.*;
+package com.gokhanaliccii.httpclient;
+
+import com.gokhanaliccii.httpclient.HttpRequestQueue.HttpRequestInfo.Method;
+import com.gokhanaliccii.httpclient.annotation.method.GET;
+import com.gokhanaliccii.httpclient.util.MockHttpRequestQueue;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class HttpClientTest {
 
+    static final String BASE_URL = "https://unsplash.com";
+
+    private HttpRequestQueue queue = null;
+
+    @Before
+    public void setUp() {
+        queue = new MockHttpRequestQueue();
+    }
+
+    @Test
+    public void should_DetermineGetMethodCorrectly() {
+        Service service = HttpClient.with(queue, BASE_URL).create(Service.class);
+
+        Request<String> call = service.getMethod();
+        HttpRequestQueue.HttpRequestInfo info = call.getRequestInfo();
+        assertThat(info.getMethod(), is(Method.GET));
+    }
+
+    interface Service {
+        @GET("/getMethod")
+        Request<String> getMethod();
+    }
 }
