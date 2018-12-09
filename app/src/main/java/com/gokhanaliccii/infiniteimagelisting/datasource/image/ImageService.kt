0284d6da39cb59fbd1711f3dfe2267fd8926355f
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-09 12:33:49 +0300

    Unsplash service model and intented endpoint information defined at ImageService

--- app/src/main/java/com/gokhanaliccii/infiniteimagelisting/datasource/image/ImageService.kt
+++ app/src/main/java/com/gokhanaliccii/infiniteimagelisting/datasource/image/ImageService.kt
@@ -1,4 +1,12 @@
 package com.gokhanaliccii.infiniteimagelisting.datasource.image
 
-class ImageService {
+import com.gokhanaliccii.httpclient.Request
+import com.gokhanaliccii.httpclient.annotation.method.GET
+import com.gokhanaliccii.httpclient.annotation.method.TYPE
+import com.gokhanaliccii.httpclient.annotation.url.Query
+
+interface ImageService {
+    @GET("/photos")
+    @TYPE(value = Image::class, isArray = true)
+    fun getImages(@Query("client_id") clientId: String): Request<Image>
 }
