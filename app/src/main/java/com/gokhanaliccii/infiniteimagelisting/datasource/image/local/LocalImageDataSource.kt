9860326434fac9ee911191df37b0d5d41baa3f39
alici,gokhan <gokhanaliccii@gmail.com>
2018-12-09 13:49:21 +0300

    LocalImageDataSource created it basically hold images and if it not exist notify image not exist

---
+++ app/src/main/java/com/gokhanaliccii/infiniteimagelisting/datasource/image/local/LocalImageDataSource.kt
@@ -0,0 +1,18 @@
+package com.gokhanaliccii.infiniteimagelisting.datasource.image.local
+
+import com.gokhanaliccii.infiniteimagelisting.datasource.image.ImageDataSource
+import com.gokhanaliccii.infiniteimagelisting.datasource.image.ImageUIModel
+import java.util.*
+
+class LocalImageDataSource : ImageDataSource {
+
+    private val pageImageListMap: Map<Int, List<ImageUIModel>> by lazy { mutableMapOf<Int, List<ImageUIModel>>() }
+
+    override fun loadImages(count: Int, page: Int, loadCallBack: ImageDataSource.ImageLoadCallBack) {
+        if (pageImageListMap.containsKey(page)) {
+            loadCallBack.onImagesLoaded(pageImageListMap.getValue(page))
+        } else {
+            loadCallBack.imagesNotExist()
+        }
+    }
+}
